301380c0cfeb9afccb97765d66049c13dbd4049d
Lukas Armstrong <Lucas.J.Armstrong@gmail.com>
2017-12-20 16:58:59 -0500

    getting time, date, and weather up and running

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const app = express()
+var weather = require('weather-js');
+var data;
+weather.find({search: '40047', degreeType: 'F'}, function(err, result) {
+  if(err) console.log(err);
+	data=result;
+	console.log(JSON.stringify(result, null, 2))
+});
 app.set('view engine', 'ejs')
 app.use(express.static('public'));
 app.get('/', function(req, res){
-	res.render('index');
+	res.render('index',{data: data});
 })
 app.listen(3000, function(){
 	console.log('Example app listening on port 3000!')
